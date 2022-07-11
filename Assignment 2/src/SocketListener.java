bf3aaefad4a73880f2811e81a6028765bc77c4c5
ale3x597 <90639588+ale3x597@users.noreply.github.com>
2022-07-11 18:06:03 -0400

    Started By adding a method that would connect to a port and display an error  if it had trouble

--- Assignment 2/src/SocketListener.java
+++ Assignment 2/src/SocketListener.java
@@ -1,9 +1,26 @@
+import java.io.*;
+import java.net.*;
+
 
 public class SocketListener {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	}
+	
+	public void ClientListener (String address, int port) {
+		try {
+			Socket socket = new Socket (address, port);
+			System.out.println("Connected");
+			
+			DataInputStream message = new DataInputStream(System.in);
+		}
+		catch (UnknownHostException e) {
+			System.out.println("Unknown Host Exception");
+		}
+		catch(IOException i) {
+			System.out.println("IO Exception");
+		}
 	}
 
 }
