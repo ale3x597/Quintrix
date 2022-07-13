8c2830eb12556d9d4f21cac38775b1792c3e874a
ale3x597 <90639588+ale3x597@users.noreply.github.com>
2022-07-13 16:17:04 -0400

    Finished initial program which checks for an incomming connection

--- Assignment 2/src/SocketListener.java
+++ Assignment 2/src/SocketListener.java
@@ -1,26 +1,42 @@
 import java.io.*;
 import java.net.*;
+import java.util.logging.Handler;
+
 
 
 public class SocketListener {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		ServerSocket server = new ServerSocket(4999);
+		while(!server.isClosed()) {
+			Socket s = server.accept();
+			Thread thread = new Handler(s);
+			thread.start();
+		}
 	}
 	
-	public void ClientListener (String address, int port) {
-		try {
-			Socket socket = new Socket (address, port);
-			System.out.println("Connected");
+	private static final class Handler extends Thread {
+		private Socket s;
+		public Handler(Socket s) {
+			this.s = s;
+		}
+		public void run() {
+			try {
+				handleConnections(s);
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
 			
-			DataInputStream message = new DataInputStream(System.in);
 		}
-		catch (UnknownHostException e) {
-			System.out.println("Unknown Host Exception");
-		}
-		catch(IOException i) {
-			System.out.println("IO Exception");
+		private void handleConnections(Socket s2) throws IOException {
+			System.out.println("Connected!");
+			
+			InputStreamReader in = new InputStreamReader(s.getInputStream());
+			BufferedReader buff= new BufferedReader(in);
+			
 		}
 	}
+	}
+	
+	
 
-}
